c24e594fce90e334e7aba4074b74c043c9fb4116
pengfei <jpf931595457@gmail.com>
2019-06-06 00:46:44 +0800

    Integrated Config Class and SQLAlchemy

--- manage.py
+++ manage.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+class Config(object):
+    """Integrated configuration Classes"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/dayanweb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+app.config.from_object(Config)
+
+# Integrated SQLAlchemy to Flask
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -9,4 +23,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
