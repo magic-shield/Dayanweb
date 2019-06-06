aa80dc18394bfbfa1f56522ab2a2ee2987f88a45
pengfei <jpf931595457@gmail.com>
2019-06-06 14:58:29 +0800

    Create app for different environments using factory pattern

--- info/__init__.py
+++ info/__init__.py
@@ -5,18 +5,29 @@ from redis import StrictRedis
 from flask_wtf import CSRFProtect
 from flask_session import Session
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-app.config.from_object(config["develop"])
 
-# Integrated SQLAlchemy to Flask
-db = SQLAlchemy(app)
+def create_app(config_name):
+    """
+    Create different app based on different environments
+    :param config_name: different configuration environments
+    :return: app
+    """
+    app = Flask(__name__)
 
-# Integrated redis
-redis_store = StrictRedis(host=config["develop"].REDIS_HOST, port=config["develop"].REDIS_PORT)
+    app.config.from_object(config[config_name])
 
-# Integrated CSRFProtect
-CSRFProtect(app)
+    # Integrated SQLAlchemy to Flask
+    db.init_app(app)
 
-# Integrated flask-session
-Session(app)
+    # Integrated redis
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+
+    # Integrated CSRFProtect
+    CSRFProtect(app)
+
+    # Integrated flask-session
+    Session(app)
+
+    return app
--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import app, db
+from info import create_app, db
+
+app = create_app("develop")
 
 # Integrated flask-script
 manager = Manager(app)
