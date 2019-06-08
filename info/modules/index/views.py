7480e0eee4e65e6dc4df2993758735fc578b2462
pengfei <jpf931595457@gmail.com>
2019-06-08 09:53:01 +0800

    Rendering Home

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,8 @@
+from flask import render_template
+
 from info.modules.index import index_blu
+
 
 @index_blu.route('/')
 def index():
-    return "hello"
+    return render_template("news/index.html")
