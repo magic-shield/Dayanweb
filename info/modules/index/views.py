137c3aa98266d3ecc6a8660aa8ea0bbc12cb7863
pengfei <jpf931595457@gmail.com>
2019-06-09 14:59:55 +0800

    Home Login Status Display

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,11 +1,25 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
+from info.models import User
 from info.modules.index import index_blu
 
 
 @index_blu.route('/')
 def index():
-    return render_template("news/index.html")
+    user_id = session.get("user_id")
+
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger(e)
+
+    data = {
+        "user_info": user.to_dict() if user else None
+    }
+
+    return render_template("news/index.html", data=data)
 
 
 @index_blu.route("/favicon.ico")
