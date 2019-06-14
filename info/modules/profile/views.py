2278e37d8b97dd6abf5f902ba2c0f024f341ab8c
pengfei <jpf931595457@gmail.com>
2019-06-14 10:19:13 +0800

    modify user base info back-end code

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,6 +1,8 @@
-from flask import g, redirect, render_template
+from flask import g, redirect, render_template, request, jsonify, current_app
 
+from info import db
 from info.utils.common import user_login
+from info.utils.response_code import RET
 from . import profile_blu
 
 
@@ -32,8 +34,32 @@ def user_base_info():
     """
     user = g.user
 
-    data = {
-        "user_info": user.to_dict()
-    }
+    if request.method == "GET":
+        data = {
+            "user_info": user.to_dict()
+        }
 
-    return render_template("news/user_base_info.html", data=data)
+        return render_template("news/user_base_info.html", data=data)
+
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    if gender not in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    user.nick_name = nick_name
+    user.signature = signature
+    user.gender = gender
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
+
+    return jsonify(errno=RET.OK, errmsg="修改成功")
