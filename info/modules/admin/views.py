07599d1e4fe688be663f4e0f760be21e2629bf31
pengfei <jpf931595457@gmail.com>
2019-06-15 19:13:37 +0800

    News review page rendering

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -3,7 +3,7 @@ import datetime
 from flask import render_template, request, current_app, session, g, redirect, url_for
 
 from info import user_login, constants
-from info.models import User
+from info.models import User, News
 from info.modules.admin import admin_blu
 
 
@@ -157,3 +157,41 @@ def user_list():
     }
 
     return render_template('admin/user_list.html', data=data)
+
+
+@admin_blu.route('/news_review')
+def news_review():
+    """
+    新闻审核页面
+    :return:
+    """
+    page = request.args.get("p", 1)
+
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        paginate = News.query.filter(News.status != 0) \
+            .order_by(News.create_time.desc()) \
+            .paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_list = []
+    for news in news_list:
+        news_dict_list.append(news.to_review_dict())
+
+    context = {"total_page": total_page, "current_page": current_page, "news_list": news_dict_list}
+
+    return render_template('admin/news_review.html', data=context)
