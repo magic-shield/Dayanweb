34eea9b16a17b523480fa49b6a17ad003b397dd2
pengfei <jpf931595457@gmail.com>
2019-06-15 11:02:33 +0800

    Add an administrator account and Manage user logins in the background

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,12 +1,38 @@
-from flask import render_template
+from flask import render_template, request, current_app, session
 
+from info.models import User
 from info.modules.admin import admin_blu
 
 
-@admin_blu.route("/login")
+@admin_blu.route("/login", methods=["GET", "POST"])
 def login():
     """
     渲染后台登录界面
     :return:
     """
-    return render_template("admin/login.html")
+
+    if request.method == "GET":
+        return render_template("admin/login.html")
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not all([username, password]):
+        return render_template('admin/login.html', errmsg="参数错误")
+
+    try:
+        user = User.query.filter(User.mobile == username, User.is_admin == 1).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template('admin/login.html', errmsg="用户信息查询失败")
+
+    if not user:
+        return render_template('admin/login.html', errmsg="未查询到用户信息")
+
+    if not user.check_passowrd(password):
+        return render_template('admin/login.html', errmsg="用户名或者密码错误")
+
+    session["user_id"] = user.id
+    session["is_admin"] = user.is_admin
+
+    return "登录成功，需要跳转到主页"
--- manage.py
+++ manage.py
@@ -12,5 +12,27 @@ manager = Manager(app)
 Migrate(app, db)
 manager.add_command("db", MigrateCommand)
 
+
+@manager.option("-n", "--username", dest="username")
+@manager.option("-p", "--password", dest="password")
+def createsuperuser(username, password):
+    if not all([username, password]):
+        print("参数不完整")
+
+    user = User()
+    user.nick_name = username
+    user.mobile = username
+    user.password = password
+    user.is_admin = 1
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+    print("成功添加管理员")
+
+
 if __name__ == '__main__':
     manager.run()
