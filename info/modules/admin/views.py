3b560069713da36356f359477f7c9a0aad3f1adf
pengfei <jpf931595457@gmail.com>
2019-06-15 19:03:39 +0800

    User list

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -2,7 +2,7 @@ import datetime
 
 from flask import render_template, request, current_app, session, g, redirect, url_for
 
-from info import user_login
+from info import user_login, constants
 from info.models import User
 from info.modules.admin import admin_blu
 
@@ -117,3 +117,43 @@ def user_count():
     }
 
     return render_template("admin/user_count.html", data=data)
+
+
+@admin_blu.route('/user_list')
+def user_list():
+    """
+    所有用户列表
+    :return:
+    """
+    page = request.args.get("page", 1)
+
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    users = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        paginate = User.query.filter(User.is_admin == 0).paginate(page, constants.ADMIN_USER_PAGE_MAX_COUNT, False)
+        users = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 进行模型列表转字典列表
+    user_dict_li = []
+    for user in users:
+        user_dict_li.append(user.to_admin_dict())
+
+    data = {
+        "users": user_dict_li,
+        "total_page": total_page,
+        "current_page": current_page,
+    }
+
+    return render_template('admin/user_list.html', data=data)
